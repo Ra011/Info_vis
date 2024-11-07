1ae7d2aced525154c1df6ffbe68fdf3a8b5509cd
raphaelattfield <raphaelattfield@hotmail.co.uk>
2024-11-07 10:54:26 +0000

    Changed precentages to be attendance rather than abscenes.

--- random_data.py
+++ random_data.py
@@ -2,9 +2,9 @@ import numpy as np
 import pandas as pd
 import statistics
 
-autumn_data_original = [5.8, 8.0, 7.6]
-spring_data_original = [3.3, 7.9, 7.0]
-summer_data_original = [5.8, 8.0, 7.6]
+autumn_data_original = [94.2, 92.0, 92.4]
+spring_data_original = [96.7, 92.1, 93.0]
+summer_data_original = [94.2, 92.0, 92.4]
 # Define the seasonal averages across all years
 autumn_mean = np.mean(autumn_data_original)  # Average for Autumn
 spring_mean = np.mean(spring_data_original)  # Average for Spring
@@ -17,10 +17,9 @@ summer_sd= statistics.stdev(summer_data_original)
 
 for i in range(1, 11):  # Loop 10 times
     # Generate 10 random absence rates around each seasonal mean
-    autumn_data = np.random.normal(autumn_mean, autumn_sd, 10).clip(0, 12)
-    spring_data = np.random.normal(spring_mean, spring_sd, 10).clip(0, 12)
-    summer_data = np.random.normal(summer_mean, summer_sd, 10).clip(0, 12)
-
+    autumn_data = np.random.normal(autumn_mean, autumn_sd, 10).clip(75, 100)
+    spring_data = np.random.normal(spring_mean, spring_sd, 10).clip(75, 100)
+    summer_data = np.random.normal(summer_mean, summer_sd, 10).clip(75, 100)
     # Arrange data in the desired layout
     transposed_data = pd.DataFrame({
         "Autumn": autumn_data,
